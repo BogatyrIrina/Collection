8ef2b09014cadb18e64a60a1131747132eb4fc42
admin <kochereva_irina@mail.ru>
2023-01-12 22:02:16 +0300

    Задание выполнено частично

---
+++ src/Mechanic.java
@@ -0,0 +1,42 @@
+public class Mechanic {
+    private String name;
+    private String surname;
+    private String company;
+
+    public Mechanic(String name, String surname, String company) {
+        this.name = name;
+        this.surname = surname;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void carryOutMaintenance (){
+        System.out.println("провести техобслуживание");
+    }
+    public void fixTheCar (){
+        System.out.println("починить машину");
+    }
+}
